0a12ac2c6d3193ed0f1f7ef7883e84e0e1d724cb
Eldar Kalachev <rusgunter@gmail.com>
2019-05-28 03:00:45 +0300

    3d day

---
+++ 1-flask-own/README.md
@@ -0,0 +1,2 @@
+To run project:
+- install 
---
+++ 1-flask-own/program/routes.py
@@ -0,0 +1,11 @@
+from program import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/tasks')
+def ptasks():
+    return render_template('tasks.html')
